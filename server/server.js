f5c5ae9c6f5dfc480003347584fd5d08bdb564bf
trungbui12 <yuuichirou67@gmail.com>
2025-11-13 18:55:53 +0700

    connectMGDB_callAPI_Clerk

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from "express"
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+connectDB()
 
 const app = express()
 app.use(cors()) // Bật chia sẻ tài nguyên giữa các nguồn khác nhau
 
-app.get('/', (req, res)=> res.send("API is working fineee "))
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res)=> res.send("API is working fine "))
 
 const PORT = process.env.PORT || 3000;
 
